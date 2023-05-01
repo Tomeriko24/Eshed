933f9ac952e3fd6a329d1c62250340922b594f61
Tomer Ronen <tomer.ronen24@gmail.com>
2023-05-01 20:03:33 +0300

    editing

--- index.js
+++ index.js
@@ -105,6 +105,21 @@ data: [
 
 
 
+function filterList() {
+  var input, filter, ul, li, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myList");
+  li = ul.getElementsByTagName("li");
+  for (var i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
 
 
 
