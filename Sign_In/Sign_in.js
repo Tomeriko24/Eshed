5cb67d971d322822e39d8ac79b0331a5af2085b1
Tomer Ronen <tomer.ronen24@gmail.com>
2023-05-24 19:52:50 +0300

    adding Dist 1

--- Sign_In/Sign_in.js
+++ Sign_In/Sign_in.js
@@ -4,8 +4,9 @@ const navLinks = document.querySelectorAll('.sidenav a');
 const users = [
   { username: "Admin", password: "Adminpswd", redirectPage: "NavBarPages/dash_board/dash_board.html"},
   { username: "Conso1", password: "Consopswd", redirectPage: "NavBarPages copy/Consultants/PagesOfConso/List Of Consts/Const1.html" },
-  { username: "Conso2", password: "Consopswd", redirectPage: "NavBarPages copy2/Consultants/PagesOfConso/List Of Consts/Const2.html" },
-  { username: "user3", password: "password3", redirectPage: "user3_page.html" },
+  { username: "Conso2", password: "Consopswd", redirectPage: "NavBarPages copy 2/Consultants/PagesOfConso/List Of Consts/Const2.html" },
+  { username: "Dist1", password: "Distpswd", redirectPage: "NavBarPages copy 3/Approval/PagesOfApproval/Dist1.html" },
+  { username: "Dist2", password: "Distpswd", redirectPage: "" },
 ];
 
 form.addEventListener('submit', (event) => {
@@ -35,4 +36,3 @@ form.addEventListener('submit', (event) => {
     form.reset();
   }
 });
-
